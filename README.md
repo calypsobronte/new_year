9929d8e0409afa202766a0b6eb8a5fe46e72c33d
Lina Maria Montaño Ramirez | calypso Bronte <codelinamaria@gmail.com>
2018-12-31 00:20:47 -0500

    New Year 2019 😍 😘 🥰

---
+++ README.md
@@ -0,0 +1,14 @@
+# Feliz año 2019 
+
+* [Codigo Codepen][]
+* [Codigo Github][]
+* [Visualizar][]
+
+
+
+
+
+<!--links -->
+[Codigo Codepen]: http://bit.ly/codepenNewYear
+[Codigo Github]: http://bit.ly/githubNewYear
+[Visualizar]: http://bit.ly/NewYear2019_
---
+++ assets/index.js
@@ -0,0 +1,169 @@
+function Particle(x, y, hu, firework) {
+    this.pos = createVector(x, y);
+    this.firework = firework;
+    this.lifespan = 255;
+    this.hu = hu;
+    this.acc = createVector(0, 0);
+    
+    if (this.firework) {
+      this.vel = createVector(0, random(-12, -8));
+    } else {
+      this.vel = p5.Vector.random2D();
+      this.vel.mult(random(2, 10));
+    }
+   
+    this.applyForce = function(force) {
+      this.acc.add(force);
+    }
+  
+    this.update = function() {
+      if (!this.firework) {
+        this.vel.mult(0.9);
+        this.lifespan -= 4;
+      }
+      
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    }
+  
+    this.done = function() {
+      if (this.lifespan < 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    this.show = function() {
+      colorMode(HSB);
+      
+      if (!this.firework) {
+        strokeWeight(2);
+        stroke(hu, 255, 255, this.lifespan);
+      } else {
+        strokeWeight(4);
+        stroke(hu, 255, 255);
+      }
+      
+      point(this.pos.x, this.pos.y);
+    }
+  }
+  
+  function Firework() {
+    this.hu = random(255);
+    this.firework = new Particle(random(width), height, this.hu, true);
+    this.exploded = false;
+    this.particles = [];
+  
+    this.done = function() {
+      if (this.exploded && this.particles.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    this.update = function() {
+      if (!this.exploded) {
+        this.firework.applyForce(gravity);
+        this.firework.update();
+        
+        if (this.firework.vel.y >= 0) {
+          this.exploded = true;
+          this.explode();
+        }
+      }
+      
+      for (var i = this.particles.length - 1; i >= 0; i--) {
+        this.particles[i].applyForce(gravity);
+        this.particles[i].update();
+        
+        if (this.particles[i].done()) {
+          this.particles.splice(i, 1);
+        }
+      }
+    }
+  
+    this.explode = function() {
+      for (var i = 0; i < 100; i++) {
+        var p = new Particle(this.firework.pos.x, this.firework.pos.y, this.hu, false);
+        this.particles.push(p);
+      }
+    }
+  
+    this.show = function() {
+      if (!this.exploded) {
+        this.firework.show();
+      }
+      
+      for (var i = 0; i < this.particles.length; i++) {
+        this.particles[i].show();
+      }
+    }
+  }
+  
+  var fireworks = [];
+  var gravity;
+  
+  function setup() {
+    var w = window.innerWidth - 40;
+    var h = window.innerHeight - 40;
+    createCanvas(w, h);
+    colorMode(HSB);
+    gravity = createVector(0, 0.2);
+    stroke(255);
+    strokeWeight(4);
+    background(0);
+  }
+  
+  function draw() {
+    colorMode(RGB);
+    background(5, 4, 4);
+    
+    if (random(1) < 0.03) {
+      fireworks.push(new Firework());
+    }
+    
+    for (var i = fireworks.length - 1; i >= 0; i--) {
+      fireworks[i].update();
+      fireworks[i].show();
+      
+      if (fireworks[i].done()) {
+        fireworks.splice(i, 1);
+      }
+    }
+    
+    var w = window.innerWidth - 40;
+    var h = window.innerHeight - 40;
+    
+    textSize(32);
+  }
+  
+  //..
+  
+  const comingdate = new Date("Jan 1, 2019 00:00:00");
+  
+  const d = document.getElementById("d");
+  const h = document.getElementById("h");
+  const m = document.getElementById("m");
+  const s = document.getElementById("s");
+  
+  let countdown = setInterval(() => {
+    const now   = new Date();
+    const des   = comingdate.getTime() - now.getTime();
+    const days  = Math.floor(des / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((des % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins  = Math.floor((des % (1000 * 60 * 60)) / (1000 * 60));
+    const secs  = Math.floor((des % (1000 * 60)) / 1000);
+  
+    d.innerHTML = getTrueNumber(days);
+    h.innerHTML = getTrueNumber(hours);
+    m.innerHTML = getTrueNumber(mins);
+    s.innerHTML = getTrueNumber(secs);
+  
+    if (countdown <= 0) clearInterval(countdown);
+  }, 1000);
+  
+  const getTrueNumber = x => (x < 10 ? "0" + x : x);
+  
